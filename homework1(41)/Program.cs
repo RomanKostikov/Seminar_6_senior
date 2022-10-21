7169b0dc8ec1edc2ac991b59ac7e84616999e975
Roman <romankos1993@gmail.com>
2022-10-21 14:01:41 +0300

    homework 1(41) completed

---
+++ homework1(41)/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Enter numbers separated by commas: ");
+    int[] numbers1 = StringToNum(Console.ReadLine()!);
+    PrintArray(numbers1);
+    int sum1 = SumPositiveNumber(numbers1);
+    Console.WriteLine($"\nNumber of positive numbers:");
+    Console.WriteLine($"{sum1}");
+
+}
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int[count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input[i] != ',')
+        {
+            if (i != input.Length - 1)
+            {
+                temp += input[i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += input[i].ToString();
+                break;
+            }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+int SumPositiveNumber(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+Main();
